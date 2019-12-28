0f3691ab2a14b2840da6d138d19b9cdf17de919e
구본철 <bbon@bbon.kr>
2019-12-28 17:44:09 +0900

    HtmlWebPackPlugin 구성 변경
    - 스크립트 태그를 추가 안함

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: 'src/index.html',
             filename: '../index.html',
+            inject: false,
         }),
     ],
     output: {
