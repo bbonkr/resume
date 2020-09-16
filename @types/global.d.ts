f571d7066b1d8f977384dfbe22e23bb688558336
Pon Cheol Ku <bbon@bbon.kr>
2020-09-17 00:07:33 +0900

    기능: add google analytics package

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,12 +1,5 @@
 export declare global {
-    interface PageViewParameter {
-        page_title: string;
-        page_location: string;
-        page_path: string;
-    }
-
     interface Window {
-        gtag: (action: string, gaId: string, parameter?: PageViewParameter) => void;
         GAID: string;
     }
 }
