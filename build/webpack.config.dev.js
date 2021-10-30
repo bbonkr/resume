4e913d8a3db0fbf4945a8d687f730d28b9687425
Pon Cheol Ku (구본철) <dev@bbon.kr>
2021-10-30 15:01:06 +0900

    change Netlify function to typescript

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -3,7 +3,7 @@ const path = require('path');
 const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const baseConfig = require('./webpack.config');
 
-const { getResume } = require('../netlify/functions/resume');
+// const { getResume } = require('../netlify/functions/resume');
 
 module.exports = {
     ...baseConfig,
@@ -28,21 +28,21 @@ module.exports = {
         static: {
             directory: path.resolve('out'),
         },
-        onBeforeSetupMiddleware: (devServer) => {
-            if (!devServer) {
-                throw new Error('webpack-dev-server is not defined');
-            }
+        // onBeforeSetupMiddleware: (devServer) => {
+        //     if (!devServer) {
+        //         throw new Error('webpack-dev-server is not defined');
+        //     }
 
-            devServer.app.get('/api/resume', async (req, res) => {
-                try {
-                    const response = await getResume('bbon');
+        //     devServer.app.get('/api/resume', async (req, res) => {
+        //         try {
+        //             const response = await getResume('bbon');
 
-                    return res.json(response);
-                } catch (error) {
-                    console.error('[DEBUG][ERROR] error', error.message);
-                    return res.status(500).json({ statusCode: 500, message: error.message });
-                }
-            });
-        },
+        //             return res.json(response);
+        //         } catch (error) {
+        //             console.error('[DEBUG][ERROR] error', error.message);
+        //             return res.status(500).json({ statusCode: 500, message: error.message });
+        //         }
+        //     });
+        // },
     },
 };
--- netlify/functions/resume.ts
+++ netlify/functions/resume.ts
@@ -1,6 +1,28 @@
-const { GraphQLClient, gql } = require('graphql-request');
+import { Handler } from '@netlify/functions';
+import { GraphQLClient, gql } from 'graphql-request';
+import { data } from '../../src/data/data';
+import { Resume } from '../../src/graphql/sdk';
 
-const getResume = async (name) => {
+// const convertDataToResume = (): { resume: Resume } => {
+//     const resume: Resume = {
+//         name: data.me?.name,
+//         username: data.me?.name,
+//         photo: data.me?.photo,
+//         homeList: data.home ? [data.home] : [],
+//         skillList: data.skillStack ? [data.skillStack] : [],
+//         contentList: [
+//             data.career ?? null,
+//             data.education ?? null,
+//             data.portfolio ?? null,
+//             data.project ?? null,
+//         ],
+//         id: 1,
+//     };
+
+//     return { resume };
+// };
+
+const getResume = async (name: string) => {
     const endpoint = process.env.ENDPOINT || '';
     const token = process.env.ACCESSKEY || '';
 
@@ -14,59 +36,19 @@ const getResume = async (name) => {
     let response;
 
     try {
-        response = await client.request(
+        response = await client.request<{ resume: Resume }>(
             gql`
-            {
-                resume: resumeFindOne(where: "(username,eq,${name})") {
-                    name
-                    username
-                    photo
-                    homeList {
-                        title
-                        subtitle
-                        intro
-                        bio
-                        linkList {
-                            title
-                            href
-                            icon
-                            target
-                            disabled
-                        }
-                    }
-                    skillList {
-                        title
-                        skill_groupList {
-                            title
-                            icon
-                            skill_itemList {
-                                title
-                                score
-                                scoremax
-                                description
-                                href
-                                disabled
-                            }
-                        }
-                    }
-                    contentList {
-                        id
-                        title
-                        content_itemList {
+                query getResume($username: String) {
+                    resume: resumeFindOne(condition: { username: { eq: $username } }) {
+                        name
+                        username
+                        photo
+                        homeList {
                             title
                             subtitle
-                            period
-                            state
-                            description
-                            disabled
-                            images
-                            content_featureList {
-                                title
-                            }
-                            content_tagMMList {
-                                title
-                            }
-                            content_linkList {
+                            intro
+                            bio
+                            linkList {
                                 title
                                 href
                                 icon
@@ -74,13 +56,56 @@ const getResume = async (name) => {
                                 disabled
                             }
                         }
+                        skillList {
+                            title
+                            skill_groupList {
+                                title
+                                icon
+                                skill_itemList {
+                                    title
+                                    score
+                                    scoremax
+                                    description
+                                    href
+                                    disabled
+                                }
+                            }
+                        }
+                        contentList {
+                            id
+                            title
+                            content_itemList {
+                                title
+                                subtitle
+                                period
+                                state
+                                description
+                                disabled
+                                images
+                                content_featureList {
+                                    title
+                                }
+                                content_tagMMList {
+                                    title
+                                }
+                                content_linkList {
+                                    title
+                                    href
+                                    icon
+                                    target
+                                    disabled
+                                }
+                            }
+                        }
                     }
                 }
-            }
-        `,
+            `,
+            { username: name },
         );
 
-        const projectItem = response.resume.contentList.find((x) => x.title === 'Project');
+        throw new Error('TEST');
+
+        const projectItem = response?.resume?.contentList?.find((x: any) => x.title === 'Project');
 
         if (projectItem) {
             const projectResponse = await client.request(
@@ -115,7 +140,9 @@ const getResume = async (name) => {
             projectItem.content_itemList = projectResponse.items;
         }
 
-        const portfolioItem = response.resume.contentList.find((x) => x.title === 'Portfolio');
+        const portfolioItem = response?.resume?.contentList?.find(
+            (x: any) => x.title === 'Portfolio',
+        );
 
         if (portfolioItem) {
             const portfolioResponse = await client.request(
@@ -150,7 +177,10 @@ const getResume = async (name) => {
             portfolioItem.content_itemList = portfolioResponse.items;
         }
     } catch {
-        throw new Error('API unavailable.');
+        // throw new Error('API unavailable.');
+        console.error('API unavailable. Use static data');
+        // return convertDataToResume();
+        return data;
     }
 
     return response;
@@ -158,7 +188,7 @@ const getResume = async (name) => {
 
 exports.getResume = getResume;
 
-exports.handler = async (event, context) => {
+const handler: Handler = async (event, context) => {
     try {
         const response = await getResume('bbon');
 
@@ -171,6 +201,7 @@ exports.handler = async (event, context) => {
                 'Content-Type': 'application/json; charset=utf-8',
                 // 'Cache-Control': `private, s-maxage=${expiration}, max-age=0`,
                 'Cache-Control': `private, max-age=${expiration}`,
+                'Access-Control-Allow-Origin': '*', // Allow from anywhere
             },
             body: JSON.stringify(response),
         };
@@ -181,3 +212,5 @@ exports.handler = async (event, context) => {
         };
     }
 };
+
+export { handler };
